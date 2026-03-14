828b567fb045f82bbe5f8ee5c384a5d51d7e83cd
Khizer002 <khizerahmed002@gmail.com>
2026-03-15 02:29:50 +0500

    Used request form for swagger

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -7,12 +7,29 @@ from fastapi.security import OAuth2PasswordRequestForm
 router=APIRouter(
     tags=["Authentication"]
 )
-@router.post("/login",response_model=schemas.Token)
-def login_user(user_credentials:schemas.Login_User,db:Session=Depends(get_db)):
-    user=db.query(models.User).filter(models.User.email==user_credentials.email).first()
+@router.post("/login", response_model=schemas.Token)
+def login_user(user_credentials: OAuth2PasswordRequestForm = Depends(),
+               db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(
+        models.User.email == user_credentials.username
+    ).first()
+
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials")
-    if not utils.pass_verify(user_credentials.password,user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Credentials")
-    jwt_token=oauth2.create_token({"sub":str(user.id)})
-    return {"access_token":jwt_token,"token_type":"bearer"}
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Credentials"
+        )
+
+    if not utils.pass_verify(user_credentials.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Credentials"
+        )
+
+    jwt_token = oauth2.create_token({"sub": str(user.id)})
+
+    return {
+        "access_token": jwt_token,
+        "token_type": "bearer"
+    }
